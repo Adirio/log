91f031472d8e9d589cdd84c7e1509b13302560d5
Adrián Orive <aorive@ikerlan.es>
2018-04-09 11:44:01 +0200

    Add initial 8 levels

---
+++ level.go
@@ -0,0 +1,58 @@
+package log
+
+type Level int
+
+const (
+	LevelAll Level = iota
+
+	// Normal behaviour
+	LevelTrace
+	LevelDebug
+	LevelInfo
+	LevelNotice
+
+	// Faulty behaviour
+	LevelWarning
+	LevelError
+	LevelCritical
+	LevelFatal
+
+	LevelNone
+)
+
+func (l Level) String() string {
+	switch l {
+	case LevelAll:
+		return "all"
+	case LevelTrace:
+		return "trace"
+	case LevelDebug:
+		return "debug"
+	case LevelInfo:
+		return "info"
+	case LevelNotice:
+		return "notice"
+	case LevelWarning:
+		return "warning"
+	case LevelError:
+		return "error"
+	case LevelCritical:
+		return "critical"
+	case LevelFatal:
+		return "fatal"
+	case LevelNone:
+		return "none"
+	default:
+		return "unknown"
+	}
+}
+
+var maxLevelLength int
+
+func init() {
+	for l := LevelAll; l <= LevelNone; l++ {
+		if maxLevelLength < len(l.String()) {
+			maxLevelLength = len(l.String())
+		}
+	}
+}
